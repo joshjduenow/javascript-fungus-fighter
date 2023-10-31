aa0f7dda4402064291777532eb85bf38ec02c49b
Joshua Duenow <joshjduenow@gmail.com>
2023-10-30 19:14:19 -0500

    add player and fungus ap

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
-// let fungusHP = 100;
+let fungusAP = 100;
+let playerAP = 100;
 
 function onReady() {
     console.log("Ready to go!")
     
+function fungusHP() {
+console.log('Fungi');
+
+}
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
     
